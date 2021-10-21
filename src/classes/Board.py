57bdb7690381ffb985359458dae8dc8e7dff7a5c
Conor <cmcnamee4@gmail.com>
2021-10-21 01:07:10 +0100

    Created skeleton of the board class

---
+++ src/classes/Board.py
@@ -0,0 +1,22 @@
+
+#Takes in the size of the board as parameters
+class Board:
+
+    #Initalised the board object and calls setBoard
+    def __init__(self, x, y):
+        self.board = []
+        setBoard(board)
+
+    # Sets the size of the board based on the x and y
+    def setBoard(self, board):
+        pass
+
+    # Returns the size of the board
+    def getBoardSize():
+        return x, y
+        pass
+    
+    # Changes the size of the board
+    def changeDimensions():
+        pass
+    
